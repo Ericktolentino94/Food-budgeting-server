9c043973e8a1dc5bb61971ba4b23f81c20a5cbab
Erick Tolentino <ericktolentino@pursuit.org>
2023-10-16 22:17:26 -0400

    added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const food = require("./controllers/foodController")
 const app = express();
 const cors = require("cors");
 
+app.use(cors())
 app.use(express.json());
 app.use("/food", food );
 
--- controllers/foodController.js
+++ controllers/foodController.js
@@ -6,6 +6,7 @@ const foodData = require("../models/food");
 
 
 food.get("/", (req, res) => {
+    console.log(foodData);
     res.json(foodData);
 })
 
@@ -13,6 +14,7 @@ food.get("/:index", (req,res) => {
     const { index } = req.params;
     console.log("SHOW ROUTE")
     if(foodData[index]) {
+        console.log(foodData[index])
         res.status(200).json(foodData[index])
     } else {
         res.status(400).send("No item found")
